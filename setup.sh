4bd205003276a3ab6898542728fa88a65b99fb7e
Willian Xavier <wlrxavier@gmail.com>
2026-05-06 00:54:11 -0300

    change to wayland/hyprland

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# ~
+#  
 ln -s $(pwd)/vim/.vimrc      $HOME/.vimrc
 ln -s $(pwd)/bash/.bashrc    $HOME/.bashrc
 ln -s $(pwd)/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/git/.gitconfig  $HOME/.gitconfig
 
-# .config
-ln -s $(pwd)/ghostty  $HOME/.config/
-ln -s $(pwd)/i3       $HOME/.config/
-ln -s $(pwd)/polybar  $HOME/.config/
-ln -s $(pwd)/picom    $HOME/.config/
-ln -s $(pwd)/rofi     $HOME/.config/
+#
+ln -s $(pwd)/kitty  $HOME/.config/
+ln -s $(pwd)/rofi   $HOME/.config/
+ln -s $(pwd)/hypr   $HOME/.config/
+ln -s $(pwd)/waybar $HOME/.config/
